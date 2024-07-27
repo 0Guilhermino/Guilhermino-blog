2ead72778237f0f5759d03c182f84d0f3a560bbc
Angelo Guilhermino <angeloguilhermino@gmail.com>
2024-07-26 21:27:36 -0300

    feat(article):implements get route articles, and get method

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,12 +1,40 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/articles', (req, res) => {
-    res.send('Rota de artigos');
-});
+const Category = require('../categories/Category');
+const Article = require('./Article');
+
+router.get('/admin/articles', (req, res) => {
+    Category.findAll({ raw: true }).then((articles) => {
+        res.render('admin/articles/index.ejs', { articles });
+    }).catch(err => {
+        console.log(err);
+        res.render('admin/articles/index.ejs');
+    });                                                                                                                                                                                    
+})
 
 router.get('/admin/articles/new', (req, res) => {
-    res.render('admin/articles/new.ejs');
+    Category.findAll().then((categories) => {
+    res.render('admin/articles/new.ejs', {categories});
+    }).catch((err) => {
+        console.log(err);
+        res.render('admin/articles/new.ejs');
+    });
+
 });
+
+router.post('/articles/save', (req, res) => {
+    let body = req.body;
+    body.slug = body.title.toLowerCase().split(' ').join('-');
+    if(body){
+        Article.create(body).then(() => {
+            res.redirect('/admin/articles');
+        }).catch(err => {
+            console.log(err);
+        })
+    } else {
+        res.redirect('/admin/articles/new');
+    }
+})
 
 module.exports = router;
