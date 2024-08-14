e1b459f3815d77923bccf5e6345979e7dd615ffb
Angelo Guilhermino <angeloguilhermino@gmail.com>
2024-08-14 16:53:41 -0300

    feat(pagination):first implement pagination

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -5,19 +5,19 @@ const Category = require('../categories/Category');
 const Article = require('./Article');
 
 router.get('/admin/articles', (req, res) => {
-    Article.findAll({ 
-        include: [{model: Category}]
+    Article.findAll({
+        include: [{ model: Category }]
     }).then((articles) => {
         res.render('admin/articles/index.ejs', { articles });
     }).catch(err => {
         console.log(err);
         res.render('admin/articles/index.ejs');
-    });                                                                                                                                                                                    
+    });
 })
 
 router.get('/admin/articles/new', (req, res) => {
     Category.findAll().then((categories) => {
-    res.render('admin/articles/new.ejs', {categories});
+        res.render('admin/articles/new.ejs', { categories });
     }).catch((err) => {
         console.log(err);
         res.render('admin/articles/new.ejs');
@@ -28,7 +28,7 @@ router.get('/admin/articles/new', (req, res) => {
 router.post('/articles/save', (req, res) => {
     let body = req.body;
     body.slug = body.title.toLowerCase().split(' ').join('-');
-    if(body){
+    if (body) {
         Article.create(body).then(() => {
             res.redirect('/admin/articles');
         }).catch(err => {
@@ -41,45 +41,45 @@ router.post('/articles/save', (req, res) => {
 
 router.post('/articles/delete', (req, res) => {
     const articleId = req.body.id;
-    
+
     console.log('entrouble article');
- 
-    if(articleId && !isNaN(articleId)){
+
+    if (articleId && !isNaN(articleId)) {
         Article.destroy({
             where: {
                 id: articleId
             }
-         }).then(() => {
+        }).then(() => {
             res.redirect('/admin/articles');
-         })
-         .catch(err => {
-            res.redirect('/admin/articles');
-            console.log(err);
-        });
-    }else {
+        })
+            .catch(err => {
+                res.redirect('/admin/articles');
+                console.log(err);
+            });
+    } else {
         res.redirect('/admin/articles');
     }
 });
 
 router.post('/articles/put', (req, res) => {
-    
+
     let body = req.body;
     body.slug = body.title.toLowerCase().split(' ').join('-');
 
     console.log(body, 'body angelo')
-    if(body){
-        Article.update(body,{
+    if (body) {
+        Article.update(body, {
             where: {
                 id: body.id
             }
-         }).then(() => {
+        }).then(() => {
             res.redirect('/admin/articles');
-         })
-         .catch(err => {
-            res.redirect('/admin/articles');
-            console.log(err);
-        });
-    }else {
+        })
+            .catch(err => {
+                res.redirect('/admin/articles');
+                console.log(err);
+            });
+    } else {
         res.redirect('/admin/articles');
     }
 });
@@ -89,15 +89,48 @@ router.get('/admin/articles/edit/:id', (req, res) => {
     const id = req.params.id;
     Article.findByPk(id).then((article) => {
         Category.findAll().then((categories) => {
-            console.log({article, categories}, '{article, categories} angelo');
-            res.render('admin/articles/update.ejs', {article, categories});
-            }).catch((err) => {
-                console.log(err);
-                res.render('admin/articles/new.ejs');
-            });
+            console.log({ article, categories }, '{article, categories} angelo');
+            res.render('admin/articles/update.ejs', { article, categories });
+        }).catch((err) => {
+            console.log(err);
+            res.render('admin/articles/new.ejs');
+        });
     }).catch(err => {
         console.log(err);
         res.render('admin/articles/index.ejs');
+    });
+})
+
+router.get('/articles/page/:num', (req, res) => {
+    const page = req.params.num;
+    const limit = 1;
+    let offset = (parseInt(page) - 1) * limit;
+
+    if (isNaN(offset) || page <= 1) {
+        offset = 0;
+    }
+
+    Article.findAndCountAll({
+        limit,
+        offset
+    }).then((articles) => {
+
+        let next = true;
+        if (offset + 1 >= articles.count) {
+            next = false;
+        }
+
+        const result = {
+            next,
+            articles
+        }
+
+        Category.findAll().then((categories) => {
+            res.render("admin/articles/page", {result, categories, });  
+        })
+    }).catch(err => {
+        console.log(err);
+        res.render('articles');
     });
 })
 
